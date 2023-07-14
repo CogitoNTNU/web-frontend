4f46899ce4be02afa4166528611b441133aa4761
Simon Sandvik Lee <simonsandviklee@gmail.com>
2023-07-14 16:26:08 +0200

    Remove scripts and add react imports

---
+++ pages/team.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Team = () => {
+    return <></>;
+};
+
+export default Team;
--- scripts/parallax.js
+++
@@ -1,9 +0,0 @@
-let xValue,
-    yValue = 0;
-
-window.addEventListener("mousemove", (e) => {
-    xValue = e.clientX;
-    yValue = e.clientY;
-
-    console.log(xValue, yValue);
-});
