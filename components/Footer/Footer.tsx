cec33040ab7ee1515a39f09bd83665a2c6c9abcd
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-01-05 15:41:52 +0100

    feat(#4): add responsive footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,35 +1,29 @@
-import styles from "../../styles/Footer.module.css";
-import {
-    AiFillInstagram,
-    AiFillFacebook,
-    AiFillLinkedin,
-    AiFillGithub,
-} from "react-icons/ai";
+import Icon from "../Icons/Icon";
 
 const Footer = () => {
     return (
-        <div className={styles.main}>
-            <div className={styles.text}>
-                <div className={styles.OrgCopy}>
+        <div className="flex h-full sm:py-[100px] py-[50px] justify-center items-center laptop:gap-[400px] tablet:gap-[250px] gap-[40px] laptop:px-[40px] px-[10px]">
+            <div className="text-white">
+                <div className="laptop:text-[20px] tablet:text-[18px] phone:text-[14px] text-[13px] font-bold">
                     <p>Cogito NTNU © 2023</p>
                 </div>
-                <div className={styles.OrgNr}>
+                <div className="laptop:text-[18px] tablet:text-[16px] text-[12px]">
                     <p>Org.Nr. 920 628 788</p>
                 </div>
             </div>
 
-            <div className={styles.icons}>
+            <div className="flex align-middle h-full gap-1 justify-center">
                 <a href="https://www.facebook.com/CogitoNTNU">
-                    <AiFillFacebook className={styles.ficons} />
+                    <Icon icon="Facebook" />
                 </a>
                 <a href="https://www.instagram.com/cogitontnu/">
-                    <AiFillInstagram className={styles.iicons} />
+                    <Icon icon="Instagram" />
                 </a>
                 <a href="https://www.linkedin.com/company/cogito-ntnu/">
-                    <AiFillLinkedin className={styles.licons} />
+                    <Icon icon="Linkedin" />
                 </a>
                 <a href="https://github.com/CogitoNTNU">
-                    <AiFillGithub className={styles.gicons} />
+                    <Icon icon="Github" />
                 </a>
             </div>
         </div>
