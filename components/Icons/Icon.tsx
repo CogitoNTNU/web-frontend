5df0dc181c0a388b10ad9569383759b236734bdb
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-01-09 15:14:53 +0100

    Feat(#3): Update Icon for hovering

--- components/Icons/Icon.tsx
+++ components/Icons/Icon.tsx
@@ -6,20 +6,35 @@ import {
     AiFillMail,
 } from "react-icons/ai";
 
+import { FiArrowRight, FiArrowLeft, FiInfo } from "react-icons/fi";
+
 interface IconProps {
     icon: string;
+    size?: string;
     color?: string;
 }
 
-const Icon = ({ icon, color }: IconProps) => {
+const Icon = ({ icon, size = "40px", color }: IconProps) => {
     return (
         <>
-            <div className="text-white text-[40px]" style={{ color: color }}>
+            <div
+                className="text-white"
+                style={{ color: color, fontSize: size }}
+            >
                 {icon == "Instagram" && <AiFillInstagram />}
                 {icon == "Facebook" && <AiFillFacebook />}
                 {icon == "Linkedin" && <AiFillLinkedin />}
                 {icon == "Github" && <AiFillGithub />}
                 {icon == "Mail" && <AiFillMail />}
+                <div className="group-hover:translate-x-2 transition-transform ease-linear tablet:block hidden">
+                    {icon == "ArrowRight" && <FiArrowRight />}
+                </div>
+                <div className="group-hover:-translate-x-2 transition-transform ease-linear tablet:block hidden">
+                    {icon == "ArrowLeft" && <FiArrowLeft />}
+                </div>
+                <div className="group-hover:scale-105 transition-transform ease-linear tablet:block hidden">
+                    {icon == "Info" && <FiInfo />}
+                </div>
             </div>
         </>
     );
