6548d35ab69113649bb6bbd84cbcbd23963cfd1a
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2024-01-21 00:56:33 +0100

    feat: :sparkles: Enter activates the onClick function in Field
    Enter activates the onClick function in Field, this requeres the enter button to be hit, and there to exist a onClick function
    
    #11

--- components/Fields/Field.tsx
+++ components/Fields/Field.tsx
@@ -2,112 +2,119 @@ import React, { useEffect, useState } from "react";
 import { FiChevronRight } from "react-icons/fi";
 
 interface FieldProps {
-    id?: string;
-    label?: string;
-    type: string;
-    placeholder?: string;
-    value: string;
-    onClick?: Function;
-    setValue: Function;
-    activeButton?: boolean;
-    errorArray?: Array<string>;
-    onChange?: (value: string) => void;
+  id?: string;
+  label?: string;
+  type: string;
+  placeholder?: string;
+  value: string;
+  onClick?: Function;
+  setValue: Function;
+  activeButton?: boolean;
+  errorArray?: Array<string>;
+  onChange?: (value: string) => void;
 }
 
 const Field = ({
-    id,
-    label,
-    type,
-    placeholder,
-    value,
-    onClick,
-    setValue,
-    activeButton = false,
-    errorArray = new Array(),
-    onChange,
+  id,
+  label,
+  type,
+  placeholder,
+  value,
+  onClick,
+  setValue,
+  activeButton = false,
+  errorArray = new Array(),
+  onChange,
 }: FieldProps) => {
-    /**
-     * Different from handle change in number fields.
-     * Adds the clicked character.
-     * @param event for when new input is added in search or text field
-     */
-    const handleChange = (
-        event:
-            | React.ChangeEvent<HTMLInputElement>
-            | React.ChangeEvent<HTMLTextAreaElement>
-    ) => {
-        const newValue = event.target.value;
-        setValue(newValue);
-        if (onChange) {
-            onChange(newValue);
-        }
-    };
+  /**
+   * Different from handle change in number fields.
+   * Adds the clicked character.
+   * @param event for when new input is added in search or text field
+   */
+  const handleChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+    if (onChange) {
+      onChange(newValue);
+    }
+  };
 
-    const [textRed, setTextRed] = useState<boolean>(false);
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && onClick) {
+      onClick();
+    }
+  };
 
-    const setError = () => {
-        if (errorArray !== null) {
-            if (errorArray.includes(id)) {
-                setTextRed(true);
-            }
-        }
-    };
+  const [textRed, setTextRed] = useState<boolean>(false);
 
-    useEffect(() => {
-        setTextRed(false);
-        setError();
-    }, [errorArray]);
+  const setError = () => {
+    if (errorArray !== null) {
+      if (errorArray.includes(id)) {
+        setTextRed(true);
+      }
+    }
+  };
 
-    return (
-        <>
-            {label && (
-                <label
-                    style={{ color: textRed ? "red" : "black" }}
-                    className="font-medium"
-                >
-                    {label}
-                </label>
-            )}
-            {type == "text" && (
-                <input
-                    className="bg-white w-full tablet:h-[50px] h-[30px] outline-none border-none border-transparent rounded-md px-4"
-                    type="text"
-                    value={value}
-                    onChange={handleChange}
-                    placeholder={placeholder}
-                />
-            )}
+  useEffect(() => {
+    setTextRed(false);
+    setError();
+  }, [errorArray]);
 
-            {type == "numbers" && (
-                <input
-                    className="bg-white w-full tablet:h-[50px] h-[30px] outline-none border-none border-transparent rounded-md px-4"
-                    type="text"
-                    inputMode="numeric"
-                    value={value}
-                    onChange={handleChange}
-                    placeholder={placeholder}
-                />
-            )}
+  return (
+    <>
+      {label && (
+        <label
+          style={{ color: textRed ? "red" : "black" }}
+          className="font-medium"
+        >
+          {label}
+        </label>
+      )}
+      {type == "text" && (
+        <input
+          className="bg-white w-full tablet:h-[50px] h-[30px] outline-none border-none border-transparent rounded-md px-4"
+          type="text"
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          placeholder={placeholder}
+        />
+      )}
 
-            {type == "area" && (
-                <textarea
-                    className="bg-white w-full h-[225px] resize-none outline-none border-none border-transparent rounded-md px-4 py-4"
-                    value={value}
-                    placeholder={placeholder}
-                    onChange={handleChange}
-                />
-            )}
+      {type == "numbers" && (
+        <input
+          className="bg-white w-full tablet:h-[50px] h-[30px] outline-none border-none border-transparent rounded-md px-4"
+          type="text"
+          inputMode="numeric"
+          value={value}
+          onChange={handleChange}
+          placeholder={placeholder}
+        />
+      )}
 
-            {activeButton && (
-                <button
-                    onClick={() => onClick()}
-                    className="bg-white rounded-md tablet:px-6 px-4 group"
-                >
-                    <FiChevronRight className="group-hover:translate-x-2 transition-transform delay-75" />
-                </button>
-            )}
-        </>
-    );
+      {type == "area" && (
+        <textarea
+          className="bg-white w-full h-[225px] resize-none outline-none border-none border-transparent rounded-md px-4 py-4"
+          value={value}
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+      )}
+
+      {activeButton && (
+        <button
+          onClick={() => onClick()}
+          className="bg-white rounded-md tablet:px-6 px-4 group"
+        >
+          <FiChevronRight className="group-hover:translate-x-2 transition-transform delay-75" />
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Field;
