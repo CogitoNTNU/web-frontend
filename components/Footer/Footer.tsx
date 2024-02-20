46fa6700b4846a5889056776bb5d7b81b7bf630f
Simon Sandvik Lee <simonsandviklee@gmail.com>
2024-02-20 11:00:55 +0100

    Feat(#36): Update navbar and footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="flex h-full sm:py-[100px] py-[50px] justify-center items-center laptop:gap-[400px] tablet:gap-[250px] gap-[40px] laptop:px-[40px] px-[10px]">
       <div className="text-white">
         <div className="laptop:text-[20px] tablet:text-[18px] phone:text-[14px] text-[12px] font-bold">
-          <p>Cogito NTNU © 2023</p>
+          <p>Cogito NTNU © 2024</p>
         </div>
         <div className="laptop:text-[18px] tablet:text-[16px] text-[10px]">
           <p>Org.Nr. 920 628 788</p>
