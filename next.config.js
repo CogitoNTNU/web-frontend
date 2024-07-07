75d065d413bf9b7e2a48eac96a51e130aa652b01
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2024-07-07 19:19:55 +0200

    feat: :sparkles: Staring to add a new endpoint for the uniqe location for marketingAI
    Staring to add a new endpoint for the uniqe location for marketingAI

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   swcMinify: true,
   env: {
     endpoint: "https://cogito-backend.net",
+    endpointMarketingAI: "https://cogito-backend.net:8001",
     google_api_key: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
   },
 };
