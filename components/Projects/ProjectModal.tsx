c8cb58fc227d0c157615ea854372a88c2c893d66
Sverre Nystad <sverrenystad@gmail.com>
2024-08-12 04:45:41 +0200

    feat: Improve ProjectModal component layout and add image loading

--- components/Projects/ProjectModal.tsx
+++ components/Projects/ProjectModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Project } from "../../lib/types";
+
 interface ProjectModalProps {
   isOpen: boolean;
   setOpen: (boolean: boolean) => void;
@@ -16,39 +17,25 @@ const ProjectModal: React.FC<ProjectModalProps> = ({
   if (!isOpen || !project) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      aria-labelledby="project-modal-title"
+      aria-modal="true"
+      role="dialog"
+      onClick={() => setOpen(false)}
+    >
       <motion.div
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.9 }}
+        transition={{ duration: 0.3 }}
         className="bg-white rounded-lg shadow-lg p-8 max-w-2xl w-full relative border-gray-darker border-2"
+        onClick={(e) => e.stopPropagation()} // Prevent click events on the modal content from closing the modal
       >
-        <div className="relative self-center w-16 h-16">
-          <Image
-            src={`/Projects/${project.image}`}
-            alt={project.name}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-full"
-          />
-        </div>
-        <h2 className="text-2xl font-bold mb-4">{project.name}</h2>
-        <p className="mb-4">{project.description}</p>
-        <p>
-          <strong>Leaders:</strong>{" "}
-          {project.leaders.length > 1
-            ? project.leaders.slice(0, -1).join(", ") +
-              " og " +
-              String(project.leaders.slice(-1))
-            : project.leaders[0]}
-        </p>
-        <p>
-          {project.workload && <strong>Workload:</strong>} {project.workload}
-        </p>
-
         <button
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700"
           onClick={() => setOpen(false)}
+          aria-label="Close modal"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -65,6 +52,41 @@ const ProjectModal: React.FC<ProjectModalProps> = ({
             />
           </svg>
         </button>
+        <div className="flex flex-col items-center mb-4">
+          <div className="relative w-32 h-32 mb-4">
+            <Image
+              src={`/Projects/${project.image}`}
+              alt={project.name}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
+            />
+          </div>
+          <h2
+            id="project-modal-title"
+            className="text-2xl font-bold text-center mb-2"
+          >
+            {project.name}
+          </h2>
+          <p className="text-gray-600 text-center mb-4">
+            {project.description}
+          </p>
+        </div>
+        <div className="text-left">
+          <p className="mb-2">
+            <strong>Leaders:</strong>{" "}
+            {project.leaders.length > 1
+              ? project.leaders.slice(0, -1).join(", ") +
+                " and " +
+                String(project.leaders.slice(-1))
+              : project.leaders[0]}
+          </p>
+          {project.workload && (
+            <p className="mb-2">
+              <strong>Workload:</strong> {project.workload} hours
+            </p>
+          )}
+        </div>
       </motion.div>
     </div>
   );
