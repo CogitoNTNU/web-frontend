ff75b9ecb75068d323c7a1821f39a6556c4cbe85
SverreNystad <sverrenystad@gmail.com>
2024-08-19 21:07:28 +0200

    refactor: move hardcoded events into data folder

---
+++ data/events.ts
@@ -0,0 +1,34 @@
+
+export const hardcodedEvents = [
+    {
+        eventName: 'Planlegging av prosjekter',
+        eventDate: '30.07',
+        eventLocation: 'Digitalt møte',
+        eventDescription: 'Planlegging av prosjekter for høstsemesteret 2024. Vi velger hvilke prosjekter som skal gjennomføres, og hvem som blir prosjektledere.',
+        eventImage: '',
+        eventLink: '',
+    },
+    {
+        eventName: 'Cogito-kveld ✨',
+        eventDate: 'Tirsdager kl. 17-20',
+        eventSubDate: 'Fra og med 27.08',
+        eventLocation: 'Gruva NTNU',
+        eventDescription: 'Hver tirsdag fra kl. 17:00 til 20:00 møtes alle i Cogito i "Gruva" for å jobbe med prosjektene våre i fellesskap. Det hender også at vi serverer pizza 🍕',
+        eventImage: '/Calendar/Cogito-night.jpg',
+    },
+    {
+        eventName: 'Bowling 🎳',
+        eventDate: 'Dato: TBD',
+        eventLocation: '',
+        eventDescription: 'Vi gjentar suksessen fra forrige semester og arrangerer en bowlingkveld for alle i Cogito. Dette er en flott mulighet til å bli kjent på tvers av prosjektene, og for å ha det gøy sammen!',
+        eventImage: '/Calendar/Bowling.jpg',
+    },
+    {
+        eventName: 'Cogito Prosjekt-presentasjoner 🎙️',
+        eventDate: '22.11',
+        eventLocation: '',
+        eventDescription: 'Presentasjoner av prosjektene Cogito-medlemmer har jobbet på dette semesteret. Dette er en flott mulighet til å se hva Cogito har jobbet med, og for å bli inspirert til å delta i prosjekter selv!',
+        eventImage: '',
+        eventLink: '',
+    },
+];
