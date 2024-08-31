3a01a56df8d0eebf40bb1b526c29b3596e9d7e6e
Sverre Nystad <sverrenystad@gmail.com>
2024-08-31 13:54:14 +0200

    refactor: apply linter and remove unused variables

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -18,7 +18,7 @@ interface FormProps {
   setIsModalOpen: Dispatch<SetStateAction<boolean>>;
   handleProjectInfoClick: (project: ProjectApply) => void;
 }
-const Form = ({ isModalOpen, selectedProject, setIsModalOpen, handleProjectInfoClick }: FormProps) => {
+const Form = ({ handleProjectInfoClick }: FormProps) => {
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
@@ -46,127 +46,132 @@ const Form = ({ isModalOpen, selectedProject, setIsModalOpen, handleProjectInfoC
     );
   };
 
-  return (<>
-    {!sent ? (<div className="bg-gray-lighter rounded-lg">
-      <div className="px-6 py-2">
-        <p className="laptop:text-[20px] text-[16px] pt-4">
-          Personlig Informasjon
-        </p>
-      </div >
-      <div className="flex laptop:flex-row flex-col">
-        <div>
-          <div className="px-6 flex gap-[10px]">
-            <div className="w-[200px] min-w-[100px] py-2">
-              <Field
-                id="first_name"
-                label="Fornavn"
-                placeholder="Cogitron"
-                value={firstName}
-                setValue={setFirstName}
-                errorArray={errorArray}
-                type={"text"}
-              />
+  return (
+    <>
+      {!sent ? (
+        <div className="bg-gray-lighter rounded-lg">
+          <div className="px-6 py-2">
+            <p className="laptop:text-[20px] text-[16px] pt-4">
+              Personlig Informasjon
+            </p>
+          </div>
+          <div className="flex laptop:flex-row flex-col">
+            <div>
+              <div className="px-6 flex gap-[10px]">
+                <div className="w-[200px] min-w-[100px] py-2">
+                  <Field
+                    id="first_name"
+                    label="Fornavn"
+                    placeholder="Cogitron"
+                    value={firstName}
+                    setValue={setFirstName}
+                    errorArray={errorArray}
+                    type={"text"}
+                  />
+                </div>
+                <div className="w-[200px] min-w-[100px] py-2">
+                  <Field
+                    id="last_name"
+                    label="Etternavn"
+                    placeholder="Cogito"
+                    value={lastName}
+                    setValue={setLastName}
+                    errorArray={errorArray}
+                    type={"text"}
+                  />
+                </div>
+              </div>
+              <div className="px-6 gap-[10px] flex-col">
+                <div className="flex-1 py-2 max-w-[410px] min-w-[100px]">
+                  <Field
+                    id="email"
+                    label="Epost"
+                    placeholder="Cogitron@cogito-ntnu.no"
+                    value={email}
+                    setValue={setEmail}
+                    errorArray={errorArray}
+                    type={"text"}
+                  />
+                </div>
+                <div className="flex-1 py-2 max-w-[410px] min-w-[100px]">
+                  <Field
+                    id="phone_number"
+                    label="Telefon"
+                    placeholder="000 00 000"
+                    value={phone}
+                    setValue={setPhone}
+                    errorArray={errorArray}
+                    type={"numbers"}
+                  />
+                </div>
+              </div>
             </div>
-            <div className="w-[200px] min-w-[100px] py-2">
+            <div className="laptop:w-[900px] laptop:min-w-[200px] max-w-[1000px] min-w-[100px] px-6 py-2">
               <Field
-                id="last_name"
-                label="Etternavn"
-                placeholder="Cogito"
-                value={lastName}
-                setValue={setLastName}
-                errorArray={errorArray}
-                type={"text"}
+                id="about"
+                label="Om deg selv"
+                value={about}
+                setValue={setAbout}
+                type={"area"}
               />
             </div>
           </div>
-          <div className="px-6 gap-[10px] flex-col">
-            <div className="flex-1 py-2 max-w-[410px] min-w-[100px]">
-              <Field
-                id="email"
-                label="Epost"
-                placeholder="Cogitron@cogito-ntnu.no"
-                value={email}
-                setValue={setEmail}
-                errorArray={errorArray}
-                type={"text"}
-              />
+          <div className="px-6 py-2">
+            <div className="w-full flex">
+              <p className="laptop:text-[20px] text-[16px] font-semibold w-full">
+                Velg Prosjekt
+              </p>
+              <p className="laptop:text-[20px] phone:text-[16px] text-[12px] justify-end flex w-full text-blue-dark">
+                Velg minst 3 prosjekter
+              </p>
             </div>
-            <div className="flex-1 py-2 max-w-[410px] min-w-[100px]">
-              <Field
-                id="phone_number"
-                label="Telefon"
-                placeholder="000 00 000"
-                value={phone}
-                setValue={setPhone}
-                errorArray={errorArray}
-                type={"numbers"}
+            <div className="tablet:py-4 py-2 w-full tablet:flex-wrap tablet:gap-x-4 flex-wrap flex">
+              {projectsApply.map((project) => (
+                <ProjectCard
+                  key={project.name}
+                  project={project}
+                  isSelected={chosenProjects.includes(project.name)}
+                  priority={
+                    chosenProjects.includes(project.name)
+                      ? chosenProjects.indexOf(project.name) + 1
+                      : null
+                  }
+                  toggleSelection={toggleProjectSelection}
+                  onInfoClick={handleProjectInfoClick}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex w-full phone:px-6 px-4 phone:py-6 py-4 laptop:text-[20px] text-[12px]">
+            <div className="flex justify-start w-full ">
+              <Button
+                text={"Send inn søknad"}
+                onClick={() => sendApplication()}
+                px={"6"}
+                py={"4"}
+                icon={"ArrowRight"}
+                color={"pink"}
+                disabled
               />
             </div>
           </div>
         </div>
-        <div className="laptop:w-[900px] laptop:min-w-[200px] max-w-[1000px] min-w-[100px] px-6 py-2">
-          <Field
-            id="about"
-            label="Om deg selv"
-            value={about}
-            setValue={setAbout}
-            type={"area"}
-          />
+      ) : (
+        <div className="tablet:h-[400px] h-[500px] w-full flex justify-center text-center items-center px-2">
+          <div>
+            <p className="font-medium tablet:text-[20px] text-[14px] tracking-wider leading-[30px]">
+              Takk for at du sendte inn søknad!
+            </p>
+            <p className="tablet:text-[16px] text-[14px]">
+              Oppmøte og datoer vil bli sendt til deg på epost/telefon snarest
+              mulig. Vi gleder oss til å møte deg!
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="px-6 py-2">
-        <div className="w-full flex">
-          <p className="laptop:text-[20px] text-[16px] font-semibold w-full">
-            Velg Prosjekt
-          </p>
-          <p className="laptop:text-[20px] phone:text-[16px] text-[12px] justify-end flex w-full text-blue-dark">
-            Velg minst 3 prosjekter
-          </p>
-        </div>
-        <div className="tablet:py-4 py-2 w-full tablet:flex-wrap tablet:gap-x-4 flex-wrap flex">
-          {projectsApply.map((project) => (
-            <ProjectCard
-              key={project.name}
-              project={project}
-              isSelected={chosenProjects.includes(project.name)}
-              priority={
-                chosenProjects.includes(project.name)
-                  ? chosenProjects.indexOf(project.name) + 1
-                  : null
-              }
-              toggleSelection={toggleProjectSelection}
-              onInfoClick={handleProjectInfoClick}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="flex w-full phone:px-6 px-4 phone:py-6 py-4 laptop:text-[20px] text-[12px]">
-        <div className="flex justify-start w-full ">
-          <Button
-            text={"Send inn søknad"}
-            onClick={() => sendApplication()}
-            px={"6"}
-            py={"4"}
-            icon={"ArrowRight"}
-            color={"pink"}
-            disabled
-          />
-        </div>
-      </div>
-    </div>) : (
-      <div className="tablet:h-[400px] h-[500px] w-full flex justify-center text-center items-center px-2">
-        <div>
-          <p className="font-medium tablet:text-[20px] text-[14px] tracking-wider leading-[30px]">
-            Takk for at du sendte inn søknad!
-          </p>
-          <p className="tablet:text-[16px] text-[14px]">
-            Oppmøte og datoer vil bli sendt til deg på epost/telefon
-            snarest mulig. Vi gleder oss til å møte deg!
-          </p>
-        </div>
-      </div>)}
-  </>)
-}
+      )}
+    </>
+  );
+};
 
 const Due = () => {
   return (
@@ -181,14 +186,20 @@ const Due = () => {
         </div>
         <div className="w-[500px] tracking-wide">
           <p className=" font-bold text-blue-darker leading-[30px]">
-             <span className="text-[16px] font-normal text-blue-darkest">Søknadsperioden for å bli med som prosjektmedlem i Cogito <span className="text-pink-default font-semibold">er nå avsluttet</span>. Men fortvil ikke! Vi vil søke etter nye prosjektmedlemmer neste semester og vær på utkikk etter verv i nær framtid.
+            <span className="text-[16px] font-normal text-blue-darkest">
+              Søknadsperioden for å bli med som prosjektmedlem i Cogito{" "}
+              <span className="text-pink-default font-semibold">
+                er nå avsluttet
+              </span>
+              . Men fortvil ikke! Vi vil søke etter nye prosjektmedlemmer neste
+              semester og vær på utkikk etter verv i nær framtid.
             </span>
           </p>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 const Apply = () => {
   // TODO: Refactor with Formik
@@ -196,7 +207,7 @@ const Apply = () => {
   const [selectedProject, setSelectedProject] = useState<ProjectApply | null>(
     null
   );
-  const dueDate = new Date("2024-08-23");
+  const dueDate = new Date("2025-08-23");
   const handleProjectInfoClick = (project: ProjectApply) => {
     setSelectedProject(project);
     setIsModalOpen(true);
@@ -205,7 +216,7 @@ const Apply = () => {
   const isDueDate = (dueDate: Date): boolean => {
     const currDate = new Date();
     return currDate > dueDate;
-  }
+  };
 
   return (
     <>
@@ -237,16 +248,21 @@ const Apply = () => {
             </p>
             <div className="w-full h-fit flex justify-center">
               <div className="w-[95%] h-fit rounded-3xl">
-                {isDueDate(dueDate) ?
-                  <Due/>
-                  :
-                  <Form handleProjectInfoClick={handleProjectInfoClick} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} selectedProject={selectedProject} />
-                }
+                {isDueDate(dueDate) ? (
+                  <Due />
+                ) : (
+                  <Form
+                    handleProjectInfoClick={handleProjectInfoClick}
+                    isModalOpen={isModalOpen}
+                    setIsModalOpen={setIsModalOpen}
+                    selectedProject={selectedProject}
+                  />
+                )}
               </div>
             </div>
-          </div >
-        </div >
-      </motion.main >
+          </div>
+        </div>
+      </motion.main>
       <ProjectModal
         isOpen={isModalOpen}
         setOpen={setIsModalOpen}
