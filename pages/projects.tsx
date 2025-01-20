307019e4597bef5bcbc9e9d0299aa17f39a7e238
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-01-20 12:36:56 +0100

    feat: Update projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,67 +4,57 @@ import Footer from "../components/Footer/Footer";
 import { motion } from "framer-motion";
 import { ProjectType } from "../lib/types";
 import { CogitoProjects } from "../data/projects";
-
+import { Carousel } from "@material-tailwind/react";
 // Images
 import Banner from "../public/Projects/MarketingAIShowcase.jpg";
 import Project from "../components/Projects/Project";
 
+
+const ProjectCarousel = () => {
+  return (
+    <div className="px-20">
+      <Carousel className="rounded-xl h-fit"
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                  }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}>
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="h-96 w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="image 2"
+          className="h-96 w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          alt="image 3"
+          className="h-96 w-full object-cover"
+        />
+      </Carousel>
+    </div>
+  );
+}
+
 const Projects = () => {
   return (
-    <div className="h-full overscroll-contain">
+    <div className="h-fit overscroll-contain animate-toblack">
       <Head>
         <title>Prosjekter - Cogito NTNU</title>
       </Head>
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 0.4,
-          ease: [0, 0.71, 0.2, 1.0],
-        }}
-        className="relative overflow-hidden"
-      >
-        <div className="relative w-full z-30 flex-none h-[720px]">
-          <div className="absolute w-full h-full bg-gradient-to-b to-transparent from-blue-dark from-0% to-30% z-50" />
-          <Image
-            className="absolute inset-0 w-full h-full object-cover shadow-2xl"
-            src={Banner}
-            alt={"banner"}
-          />
-        </div>
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.6,
-            delay: 0.4,
-            ease: [0, 0.71, 0.2, 1.0],
-          }}
-          className="absolute z-50 w-full flex items-end justify-center phone:top-[700px] top-[650px]"
-        >
-          <p className="bg-gray-default px-8 rounded-2xl absolute font-bold text-white laptop:text-[80px] tablet:text-[70px] text-[42px] tracking-wid drop-shadow-3xl">
-            PROSJEKTER
-          </p>
-        </motion.div>
-
-        <div className="flex py-6 w-full">
-          <p className="text-center w-full phone:text-[20px] text-[16px] text-white">
-            Her er noen av prosjektene som har blitt laget <br /> av Cogitos
-            medlemmer
-          </p>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="my-2 w-[70%] h-[2px] bg-gray-lighter"></div>
-        </div>
-
-        <div className="pt-8 flex justify-center">
-          <div className="grid gap-8">
-            {CogitoProjects.map((project: ProjectType) => (
-              <Project key={project.github} {...project} />
-            ))}
-          </div>
-        </div>
-      </motion.main>
+      <main className="h-screen w-full pt-44 justify-center flex">
+        <ProjectCarousel />
+      </main>
       <Footer />
     </div>
   );
