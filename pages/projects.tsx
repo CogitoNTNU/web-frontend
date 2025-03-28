b3e45cb7cc49dba0f6d3a81a7d9e21014d9c3f08
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-28 02:13:18 +0100

    refactor: Add new projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,11 +8,11 @@ import { Carousel } from "@material-tailwind/react";
 // Images
 import Banner from "../public/Projects/MarketingAIShowcase.jpg";
 import Project from "../components/Projects/Project";
-
+import Navbar from "../components/Navbar/Navbar";
 
 const ProjectCarousel = () => {
   return (
-    <div className="px-20">
+    <>
       <Carousel className="rounded-xl h-fit"
         navigation={({ setActiveIndex, activeIndex, length }) => (
           <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
@@ -25,24 +25,24 @@ const ProjectCarousel = () => {
               />
             ))}
           </div>
-        )}>
+        )} autoplay loop>
         <img
           src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
           alt="image 1"
-          className="h-96 w-full object-cover"
+          className="h-[30rem] w-full object-cover"
         />
         <img
           src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
           alt="image 2"
-          className="h-96 w-full object-cover"
+          className="h-[30rem] w-full object-cover"
         />
         <img
           src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
           alt="image 3"
-          className="h-96 w-full object-cover"
+          className="h-[30rem] w-full object-cover"
         />
       </Carousel>
-    </div>
+    </>
   );
 }
 
@@ -52,8 +52,16 @@ const Projects = () => {
       <Head>
         <title>Prosjekter - Cogito NTNU</title>
       </Head>
-      <main className="h-screen w-full pt-44 justify-center flex">
+      <Navbar page="projects"/>
+      <main className="h-fit w-full pt-44 justify-center items-center flex-col px-20">
         <ProjectCarousel />
+        <div className="pt-16 pb-12">
+          <p className="text-white font-semibold text-3xl tracking-wide">Prosjekt Galleri</p>
+          <div className="flex pt-8 gap-x-12 gap-y-4 flex-wrap">
+            <Project name="Marketing AI" github="www.github.com" img="/Projects/Cards/MarketingAI.png" playable url={"/projects/marketingai"}/>
+            <Project name="NEATactics" github="www.github.com" img="/Projects/Cards/NEATactics.png" released={false}  url={"/projects/articles/neatactics"}/>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
