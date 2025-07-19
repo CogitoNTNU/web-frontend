f6aeca179eb86d70a9f10c0de2e6b045ce3edb4c
Sverre Nystad <sverrenystad@gmail.com>
2025-07-19 18:24:13 +0200

    fix: adjust layout of ProjectFilterChips to make them wrap around

--- components/team/ProjectFilterChips.tsx
+++ components/team/ProjectFilterChips.tsx
@@ -17,7 +17,7 @@ export default function ProjectFilterChips({
     <div className="relative mx-auto mb-10 sm:mb-12 w-full max-w-full overflow-x-auto scrollbar-hide">
       <div className="pointer-events-none absolute left-0 top-0 h-full w-6 bg-gradient-to-r from-gray-lighter to-transparent" />
       <div className="pointer-events-none absolute right-0 top-0 h-full w-6 bg-gradient-to-l from-gray-lighter to-transparent" />
-      <div className="flex w-max gap-2 px-4 pr-8 sm:gap-3 sm:px-6 justify-center mx-auto">
+      <div className="flex flex-wrap gap-2 px-4 pr-8 sm:gap-3 sm:px-6 justify-center items-center w-full">
         <Chip
           label="All Projects"
           active={projectFilter == null}
