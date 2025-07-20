fc72ccd0fb374acde28eb2e1b2dfe27ec2a8af14
Sverre Nystad <sverrenystad@gmail.com>
2025-07-20 11:37:01 +0200

    refactor: clean up useGetMembers hook and restore API calls for members and projects

--- hooks/useGetMembers.tsx
+++ hooks/useGetMembers.tsx
@@ -1,47 +1,24 @@
 import axios from "axios";
-import { Member, Project } from "../lib/types";
 import { useQuery } from "@tanstack/react-query";
-import { memberQueryKey } from "../lib/queryKeys";
+import { Member, Project } from "../lib/types";
 
-// const getMembers = async (params: GetMembersType) => {
-//   const { data } = await axios.get<MemberType[]>(
-//     `${process.env.endpoint}/api/members-by-type/`,
-//     {
-//       params,
-//     }
-//   );
-//
-//   return data;
-// };
-//
-// export const useGetMembers = (params: GetMembersType) => {
-//   return useQuery<MemberType[]>({
-//     queryKey: [memberQueryKey(params)],
-//     queryFn: () => getMembers(params),
-//     enabled: !!params,
-//   });
-// };
+const fetchMembers = async (): Promise<Member[]> => {
+  const { data } = await axios.get<Member[]>(
+    `${process.env.endpoint}/api/members-by-type/`,
+    { params: { member_type: "Alle Medlemmer" } }
+  );
+  return data;
+};
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
-// const fetchMembers = async (): Promise<Member[]> => {
-//   const { data } = await axios.get<Member[]>(
-//     `${API_URL}/api/members-by-type/`,
-//     { params: { member_type: "Alle Medlemmer" } }
-//   );
-//   return data;
-// };
-// const fetchProjects = async (): Promise<ProjectBrief[]> => {
-//   const { data } = await axios.get<ProjectBrief[]>(`${API_URL}/api/projects/`);
-//   return data;
-// };
-
-import membersMock from "../data/members.json";
-import projectsMock from "../data/projects.json";
-
-const fetchMembers = async () => membersMock as Member[];
-const fetchProjects = async () => projectsMock as Project[];
+const fetchProjects = async (): Promise<Project[]> => {
+  const { data } = await axios.get<Project[]>(
+    `${process.env.endpoint}/api/projects/`
+  );
+  return data;
+};
 
 export const useAllMembers = () =>
   useQuery({ queryKey: ["members"], queryFn: fetchMembers });
+
 export const useAllProjects = () =>
   useQuery({ queryKey: ["projects"], queryFn: fetchProjects });
